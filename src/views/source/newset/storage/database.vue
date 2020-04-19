c0924ad80bf14917f35dfb08d3e650f63da1545f
yuancheng <1963312192@qq.com>
2020-04-19 16:15:22 +0800

    共享文档表单提交

---
+++ src/views/source/newset/storage/database.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <common-form />
+  </div>
+</template>
+<script>
+import commonForm from './components/common-form'
+
+export default {
+  name: 'Database',
+  components: {
+    commonForm
+  },
+  data() {
+    return {
+
+    }
+  },
+
+  created() {},
+
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          alert('submit!')
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields()
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.tree-form-item {
+    padding: 25px 0;
+  >>> .el-card__header {
+    padding: 0 20px;
+    background-color: #ededed;
+  }
+}
+</style>
