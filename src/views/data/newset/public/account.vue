8b97d46671ab2f7e02095616cc2bca4a45e41617
viki <2320450659@qq.com>
2020-05-13 22:23:41 +0800

    新建公共信息源目录

---
+++ src/views/data/newset/public/account.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+  </div>
+</template>
+<script>
+export default {
+  name: "account",
+  components: {
+  },
+  data() {
+    return {
+    };
+  },
+
+  created() { },
+
+  methods: {}
+};
+</script>
+
+<style lang="scss" scoped></style>
